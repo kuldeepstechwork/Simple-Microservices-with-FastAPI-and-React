40a4d140ea66b024cb0486c0ea26722190532e43
KuldeepstechWork <kuldeep.cse12@gmail.com>
2023-02-01 22:08:04 +0530

    able to palce the order and applied async fyn to updateorder status

---
+++ Inventory/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+
+)
+
+
+redis = get_redis_connection(
+    host = "redis-16426.c16.us-east-1-2.ec2.cloud.redislabs.com",
+    port = 16426,
+    password = "MUZr8UnXgMmR1Kd8p8WcqtpLWcJ2Q6Ti",
+    decode_responses = True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return{
+        'id':product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
---
+++ Payment/main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+import requests
+import time
+from fastapi.background import BackgroundTasks
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+
+)
+
+#each microservice need its own database
+redis = get_redis_connection(
+    host = "redis-16426.c16.us-east-1-2.ec2.cloud.redislabs.com",
+    port = 16426,
+    password = "MUZr8UnXgMmR1Kd8p8WcqtpLWcJ2Q6Ti",
+    decode_responses = True
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str #pending, completed, refund
+
+    class Meta:
+        database = redis
+
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk)
+
+
+@app.post('/orders')
+async def create(request:Request, background_tasks: BackgroundTasks): #id, quantity
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' %body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2*product['price'],
+        total = 1.2*product['price'],
+        quantity = body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    background_tasks.add_task(order_completed, order)
+
+    return order 
+
+def order_completed(order:Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
